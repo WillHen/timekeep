5bc6f6ea0d5c054f06a628471d82fd5f96444e52
Will H <whenshaw87@googlemail.com>
2015-04-30 14:52:31 +0100

    include images on watch model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430110456) do
+ActiveRecord::Schema.define(version: 20150430135119) do
 
   create_table "watches", force: true do |t|
     t.string   "name"
@@ -19,11 +19,15 @@ ActiveRecord::Schema.define(version: 20150430110456) do
     t.integer  "price_in_pence"
     t.string   "style"
     t.string   "gender"
-    t.boolean  "pre_owned",      default: false
+    t.boolean  "pre_owned",          default: false
     t.integer  "user_id"
     t.string   "brand"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
