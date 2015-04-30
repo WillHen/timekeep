a2d0d155308a7ddec1bd2f194617d0bacc2aea9e
Will H <whenshaw87@googlemail.com>
2015-04-30 11:14:17 +0100

    Assign dummy wacth data

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,5 +1,9 @@
 class WatchesController < ApplicationController
   def index
+      @watches = [{name: "Backup Black", description: "lorem ipson", price_in_pence: 5000}, 
+          {name: "Classic Digitial Casino Silver/Yellow", description: "lorem ipson", price_in_pence: 4999}, 
+              {name: "1960s vintage Raketa", description: "lorem ipson", price_in_pence: 12000}, 
+          {name: "Max Bill Chronoscope", description: "lorem ipson", price_in_pence: 200000}]
   end
 
   def show
@@ -7,7 +11,16 @@ class WatchesController < ApplicationController
 
   def new
   end
+  
+  def create
+  end
 
   def edit
   end
+    
+   def update
+   end
+    
+    def destroy
+   end
 end
